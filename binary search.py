ec37527049862d629dd12a36dbb9ffa36895c346
rashmisahray <152407465+rashmisahray@users.noreply.github.com>
2024-06-07 17:15:22 +0530

    file by rashmi

---
+++ binary search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+# Test the function
+print(binary_search([1, 2, 3, 4, 5, 6], 4))  
