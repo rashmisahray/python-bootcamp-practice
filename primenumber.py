448336e11b6bc4e554939d5d9b5ed2b529b7877e
rashmisahray <152407465+rashmisahray@users.noreply.github.com>
2024-06-07 16:19:54 +0530

    created by rashmi

---
+++ primenumber.py
@@ -0,0 +1,11 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Test the function
+print(is_prime(17)) 
+print(is_prime(15))  
